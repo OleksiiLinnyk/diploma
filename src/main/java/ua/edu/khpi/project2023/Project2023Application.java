eb76120953a9ad0e999d62ac7c9b7f6445a8791f
Vvirich <virichbog@gmail.com>
2023-05-01 16:03:33 +0300

    first commit

---
+++ src/main/java/ua/edu/khpi/project2023/Project2023Application.java
@@ -0,0 +1,13 @@
+package ua.edu.khpi.project2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/khpi/project2023/Project2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.khpi.project2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
