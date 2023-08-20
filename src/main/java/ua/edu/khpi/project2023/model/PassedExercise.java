1403eb98d892e39cc45fd3c23527ee6dbb7a79fe
Vvirich <virichbog@gmail.com>
2023-08-20 17:46:13 +0300

    Added Entity annotayion

--- src/main/java/ua/edu/khpi/project2023/model/PassedExercise.java
+++ src/main/java/ua/edu/khpi/project2023/model/PassedExercise.java
@@ -3,11 +3,13 @@ package ua.edu.khpi.project2023.model;
 import lombok.*;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 
 @EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
+@Entity
 public class PassedExercise extends Exercise {
 
     @Column(name = "user_id")
